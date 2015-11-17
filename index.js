c31c7c065e344525f12c94681b750caeedbaac43
Sindre Sorhus <sindresorhus@gmail.com>
2015-11-17 17:18:34 +0700

    handle the case when there's no editor

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 module.exports = function (text, editor) {
 	editor = editor || atom.workspace.getActiveTextEditor();
 
+	if (!editor) {
+		return;
+	}
+
 	const editorEl = atom.views.getView(editor);
 	const cursorPosition = editor.getCursorBufferPosition();
 	const line = editorEl.getFirstVisibleScreenRow() + editor.displayBuffer.getVerticalScrollMargin();
